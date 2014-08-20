a7dbf8a4855278f2facbbb04393f675899850b8a
TomInIreland <thomasht0423@gmail.com>
2014-08-20 16:43:01 +0100

    commit2

---
+++ js/THscript.js
@@ -0,0 +1,55 @@
+// Tao's JavaScript
+var lnStickyNavigation;
+
+$(document).ready(function()
+{	
+	applyNavigation(); 
+});
+/* NAVIGATION FUNCTIONS */
+
+function applyNavigation()
+{
+	applyStickyNavigation();
+	applyClickEvent();
+}
+
+function applyClickEvent()
+{
+	$('a[href*=#]').on('click', function(e)
+	{
+		e.preventDefault();
+		
+		if( $( $.attr(this, 'href') ).length > 0 )
+		{
+			$('html, body').animate(
+			{
+				scrollTop: $( $.attr(this, 'href') ).offset().top
+			}, 400);
+		}
+		return false;
+	});
+}
+
+function applyStickyNavigation()
+{
+	lnStickyNavigation = $('.nav').offset().top ;
+	
+	$(window).on('scroll', function() 
+	{  
+		stickyNavigation();  
+	});  
+	
+	stickyNavigation();
+}
+
+function stickyNavigation()
+{         
+	if($(window).scrollTop() > lnStickyNavigation) 
+	{   
+		$('body').addClass('fixed');  
+	} 
+	else 
+	{  
+		$('body').removeClass('fixed');   
+	}  
+}
